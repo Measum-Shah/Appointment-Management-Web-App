32bf055f8f4c00e1ef8ced7f4008fdca7e8afc74
Measum-Shah <shahmeasum008@gmail.com>
2025-08-08 17:48:48 +0500

    made Navbar

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react'
+import {assets} from "../assets/assets_frontend/assets"
+import { NavLink, useNavigate } from 'react-router-dom'
+
+const Navbar = () => {
+  const navigate = useNavigate();
+  const [showMenu,setShowMenu] = useState(false);
+  const  [token,setToken] = useState(true);
+  return (
+    <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400'>
+        <img className='w-44' src={assets.logo} alt="" />
+        <ul className='hidden md:flex items-start gap-5 font-medium'>
+            <NavLink to='/'>
+                <li className='py-1'>HOME</li>
+                <hr className='md:hidden block border-none w-3/5 m-auto 
+                 outline-none h-0.5 bg-primary'/>
+            </NavLink>    
+            <NavLink to='/doctors'> 
+                <li className='py-1'>ALL DOCTORS</li>
+                <hr className='md:hidden block w-3/5 m-auto border-none
+                 outline-none h-0.5 bg-primary'/>
+             </NavLink>
+             <NavLink to= '/about'>    
+                <li className='py-1'>ABOUT</li>
+                <hr className='md:hidden block w-3/5 m-auto  border-none
+                 outline-none h-0.5 bg-primary'/>
+             </NavLink>  
+             <NavLink to='/contact'>
+                <li className='py-1'>CONTACT</li>
+                <hr className='md:hidden block border-none w-3/5 m-auto 
+                 outline-none h-0.5 bg-primary'/>                              
+             </NavLink>  
+        </ul>
+        <div className='flex items-center gap-4'>
+            {
+              token
+              ?
+               <div className='flex gap-1 items-center cursor-pointer group relative'>
+                <img className='w-8 rounded-full'  src={assets.profile_pic} alt="profile-picture" />
+                <img className='w-2.5' src={assets.dropdown_icon} alt="dropdown-icon" />
+               <div className='absolute top-0 right-0 pt-14 
+               font-medium text-gray-600 text-base hidden group-hover:block '>
+                <div  className='min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4'>
+                  <p onClick={()=>navigate('/my-profile')} className='hover:text-black hover:cursor-pointer'>My Profile</p>
+                  <p onClick={()=>navigate('/my-appointments')} className='hover:text-black hover:cursor-pointer'>My Appointment</p>
+                  <p onClick={()=>setToken(false)} className='hover:text-black hover:cursor-pointer'>Logout</p>
+                </div>
+               </div>
+               </div>
+              :
+               <button onClick={()=>navigate('/login')} className='bg-primary text-white px-8 py-3 
+               rounded-full font-light hidden md:block'>
+               Create account
+               </button>
+            }
+        </div>
+    </div>
+  )
+}
+
+export default Navbar

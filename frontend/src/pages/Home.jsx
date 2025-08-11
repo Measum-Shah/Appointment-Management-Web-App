f7d9cc4a8b062e749445e359ca836b22e8d16616
Measum-Shah <shahmeasum008@gmail.com>
2025-08-11 16:01:48 +0500

    Made top doctors component and cone=text to provide doctors value

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Header from '../components/Header'
 import SpeciallityMenu from '../components/SpeciallityMenu'
+import TopDoctors from '../components/TopDoctors'
+
 
 const Home = () => {
   return (
     <>
     <Header />
     <SpeciallityMenu />
+    <TopDoctors/>
     </>
   )
 }
